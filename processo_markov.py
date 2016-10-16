7dd791bba101e9960f4a2653c95b12f19934de33
Matheus Menezes <matheuschavesmenezes@gmail.com>
2016-10-16 08:45:13 -0700

    processo de markov

--- processo_markov.py
+++ processo_markov.py
@@ -1,6 +1,8 @@
 import numpy as np
 
 def vetor_ini():
-    print 'Entre com o vetor de inicializacao: '
+    print 'Entre com o vetor de inicializacao com espacos para cada elemento: '
+    ini = input()
+    print ini
 
 vetor_ini()
