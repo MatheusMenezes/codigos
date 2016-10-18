19a7b4637eb0f8b2087f28fb8f612865460c22b6
Matheus Menezes <matheuschavesmenezes@gmail.com>
2016-10-18 00:19:58 -0300

    pm

--- processo_markov.py
+++ processo_markov.py
@@ -11,23 +11,25 @@ def verify(a):
         return True
 
 def vetor_ini(n):
-    vetor = list()
-    print 'Entre com o vetor de inicializacao: '
+    vetor = np.zeros(n)
+    while not verify(vetor):
+        print 'Entre com o vetor de inicializacao: '
+        for i in range(int(n)):
+            valor_ini = input("pos[%d]: " %i) #porcentagem
+            vetor[i] = valor_ini
 
-    for i in range(int(n)):
-        porc = input("pos[%d]: " %i) #porcentagem
-        vetor.append(porc)
-
-    print 'Vetor de inicialicazao: ',vetor
+    print 'VETOR DE INICIALIZACA: ',vetor
     return vetor
 
 
-def matriz_transacao(n):
+def matriz_transicao(n):
     matriz = np.zeros((n, n))
+    print 'Entre com a matriz de transicao: '
     for x in range(int(n)):
-        for y in range(int(n)):
-            print ('matriz['+str(x)+']['+str(y)+']')
-            matriz[x][y] = input()
+        while not verify(matriz[x]):
+            for y in range(int(n)):
+                matriz[x][y] = input('pos['+str(x)+']['+str(y)+']: ')
+    print 'MATRIZ DE TRANSICAO: \n',matriz
     return matriz
 
 def exp_matriz(h, m):
@@ -41,20 +43,29 @@ def multi_exp_ini(ini, exp):
 
 
 if __name__ == '__main__':
-    n = input('Entre com o tamanho do vetor: ')
+    n = input('Entre com o tamanho do vetor de inicializacao: ') #tamanho do vetor
+    a = np.zeros(n) #vetor de inicialicacao
+    m = np.zeros((n,n)) #matriz de transicao
+    h = 0 #numero de passos
+    exp = np.zeros # (a * m^h)
+    flag_a = flag_m = flag_h = False #flags de controle de alteracao das variaveis
+    #opt = 1 #opcao dentro do loop
+    opt = input('1-Definir Vetor de Incialicazao\n2-Definir Matriz de Transicao\n3-Definir numero de passos\n4-Definir qual observar\n0-Sair\nopcao: ')
+    while opt:
+        if opt == 1: #definir vetor de inicialicacao
+            a = vetor_ini(n)
+            flag_a = True
+        if opt == 2: #definir matriz de transicao
+            m = matriz_transicao(n)
+            flag_m = True
+        if opt == 3: #definir numero de passos
+            h = input ('Entre com o numero de passos: ')
+            flag_h = True
+        if opt == 4: #definir qual elemento observar
+            exp = 0
+            #if flag_a and flag_m and flag_h:
+            exp = multi_exp_ini(a, exp_matriz(h, m))
+            print 'Exp ', exp
+        opt = input('1-Definir Vetor de Incialicazao\n2-Definir Matriz de Transicao\n3-Definir numero de passos\n4-Definir qual observar\n0-Sair\nopcao: ')
 
-    a = vetor_ini(n)
-    while verify(a):
-        print 'probabilidades do vetor diferente de 1.0 (100%)\nrefaca o processo ate soma das prob. igual a 1'
-        a = vetor_ini(n)
-
-    m = matriz_transacao(n)
-    exp = exp_matriz(4, m)
-
-    print exp
-
-
-
-
-#matriz_transicao(3)
 
