35414f2601344ba51837acccad4834d7c3486bee
Matheus Menezes <matheuschavesmenezes@gmail.com>
2016-12-02 22:38:55 -0300

    ce

--- class_est_cadeia.py
+++ class_est_cadeia.py
@@ -15,14 +15,14 @@ def verifica_regular():
 	soma = 0
 	teste = 0
 	g=2
-	s=0 
-	
+	s=0
+
 	n = input ("Dimensao (n) da matriz: ")
 	m = np.zeros((n, n))
 	matriz = np.zeros((n,n))
 	for i in range (n):
 		for j in range (n):
-			print str(i) + " " + str(j) + ": "
+			print "matriz " + str(i) + " " + str(j) + ": "
 			m[i][j] = input()
 			if matriz[i][j]<=-1:
 				teste=1
@@ -47,28 +47,28 @@ def verifica_regular():
 	    for i in range(n):
 	        for j in range(n):
 	            m[i][j] = c_kolmogorov(matriz, n, p, i, j);
-	            
+
 	            if m[i][j]<= -1:
-	            	teste = 1 
-	    
+	            	teste = 1
+
 	    for i in range (n):
 	        soma=0
 	        for j in range(n):
-	            soma = soma+m[i][j]    
+	            soma = soma+m[i][j]
 	        if soma!=1:
 	        	s = 1
-	    
+
 
 	    if teste==1 or s==1:
 	    	print "GERACAO " + str(g)+ " - NAO REGULAR\n"
 	    else:
-	    
+
 	        print "GERACAO " + str(g)+ " - REGULAR\n"
-	    
+
 	    for i in range (n):
 	        for j in range(n):
-	            matriz[i][j]=m[i][j]   
+	            matriz[i][j]=m[i][j]
 	    p += 1
 	    g += 1
-    
+
 verifica_regular()
